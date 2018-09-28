7eadd3ac0a6d4ba90ee16f8ebd2d8667550bec4c
paulo-SF <paulosoaresferreira@gmail.com>
2018-09-28 14:52:22 -0300

    Base Bootstrap Cadastro de  Empresa e Lançamento

--- software-lancamento-ui/src/app/app.module.ts
+++ software-lancamento-ui/src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { EmpresasPesquisaComponent } from './empresas-pesquisa/empresas-pesquisa.component';
+import { LancamentoCadastroComponent } from './lancamento-cadastro/lancamento-cadastro.component';
+import { EmpresasCadastroComponent } from './empresas-cadastro/empresas-cadastro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LancamentosPesquisaComponent,
     NavbarComponent,
-    EmpresasPesquisaComponent
+    EmpresasPesquisaComponent,
+    LancamentoCadastroComponent,
+    EmpresasCadastroComponent
   ],
   imports: [
     BrowserModule
